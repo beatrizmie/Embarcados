f8bd70ab8d689b8a818c8e2c9b27a17d205eabf4
beatrizmie <38322370+beatrizmie@users.noreply.github.com>
2020-05-06 17:36:28 -0300

    Lab FIR

--- RTOS-ADC/src/main.c
+++ RTOS-ADC/src/main.c
@@ -56,7 +56,7 @@ struct ili9488_opt_t g_ili9488_display_opt;
 #define NUM_TAPS   12  // ordem do filtro (quantos coefientes)
 #define BLOCK_SIZE 1   // se será processado por blocos, no caso não.
 
-
+//filtro de ordem 8
 const float32_t firCoeffs32[NUM_TAPS] ={0.12269166637219883,
 	0.12466396327768503,
 	0.1259892807712678,
@@ -66,6 +66,7 @@ const float32_t firCoeffs32[NUM_TAPS] ={0.12269166637219883,
 	0.12466396327768503,
 0.12269166637219883};
 
+//filtro de ordem 12
 const float32_t filtroOrdem12[NUM_TAPS] = {0.07930125683894955,
 	0.08147535648783032,
 	0.08323976516671625,
